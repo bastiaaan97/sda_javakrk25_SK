27e3a6649dd9119ebac159712203776b4b54127d
Uzytkownik <uzytkownik@kursGIT.pl>
2020-01-25 10:22:29 +0100

    additional comment

--- src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
+++ src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
@@ -4,6 +4,12 @@ import java.util.Random;
 
 public class Zad_14_histogram {
 
+    /**
+     * Stworzyć program który generuje histogram losowych liczb
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
         int[] histogram = new int[10];
         Random random = new Random();
