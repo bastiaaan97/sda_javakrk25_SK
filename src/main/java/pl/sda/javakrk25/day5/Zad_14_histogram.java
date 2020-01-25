a8aa2b2d32ee54a5e6f69d352e5afee0238b469d
Uzytkownik <uzytkownik@kursGIT.pl>
2020-01-25 10:58:20 +0100

    nowy

--- src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
+++ src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
@@ -6,6 +6,7 @@ public class Zad_14_histogram {
 
     /**
      * Stwórz program który generuje histogram losowych liczb
+     * TEST BRANCH CHANGE
      *
      * @param args
      */
@@ -23,5 +24,6 @@ public class Zad_14_histogram {
         for (int i = 0; i < histogram.length; i++) {
             System.out.println(String.format("%d -> %d", i, histogram[i]));
         }
+        // additional something
     }
 }
