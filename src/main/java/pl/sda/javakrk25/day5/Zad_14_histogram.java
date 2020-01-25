d106c7e8993fc773ab50194415792c788223ebf3
Uzytkownik <uzytkownik@kursGIT.pl>
2020-01-25 10:33:27 +0100

    typo

--- src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
+++ src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Zad_14_histogram {
 
     /**
-     * Stworzyć program który generuje histogram losowych liczb
+     * Stwórz program który generuje histogram losowych liczb
      *
      * @param args
      */
